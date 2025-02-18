ce0aca38ac0ade0c083c4f8f81a6691cf2ffcb3a
lzyprime <liuzhiyuan@uniontech.com>
2025-02-18 21:40:38 +0800

    1. 优化 properties 参数初始化, 判断当前是否已经配置过参数化构建.    避免重复配置 2. gh_actions 增加SET_LABELS参数, 会清空目标所有label, 然后设置新label.    不可与ADD_LABELS,REMOVE_LABELS混用. 3. 修复流水线执行过程中bug. 修改参数存储与读取方式

--- RVCI/RVCK/gh_actions/run.sh
+++ RVCI/RVCK/gh_actions/run.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 set -e
 
+CONFIG_all_labels="kernel_build_failed,kernel_build_succeed,kernel_building,kernel_waiting,lava_check_fail,lava_check_pass,lava_checking,lava_waiting"
+
+
 if [ "$REPO" = "" ] || [ "$ISSUE_ID" = "" ]; then
     echo "'REPO' and 'ISSUE_ID' is required"
     exit 1
 fi
 
 
+if [ "$ADD_LABLE" != "" ] || [ "$REMOVE_LABEL" != "" ]; then
+    if [ "$SET_LABEL" != "" ]; then
+        echo "SET_LABEL 和 ADD_LABEL,REMOVE_LABEL 不能同时使用"
+        exit 1
+    fi
+fi
+
+set -x
 if [ "$COMMENT_CONTENT" != "" ]; then
-    #gh issue view "$ISSUE_ID" --json comments --jq '.comments|.[-1]|.body' -R "$REPO"
     gh issue comment "$ISSUE_ID" -b "$COMMENT_CONTENT" -R "$REPO"
-    #gh issue view "$ISSUE_ID" --json comments --jq '.comments|.[-1]|.body' -R "$REPO"
-    #set +x
 fi
 
 if [ "$REMOVE_LABEL" != "" ]; then
-    set -x
-    #gh issue view "$ISSUE_ID" --json labels --jq '.labels|.[]|.name' -R "$REPO"
     gh issue edit "$ISSUE_ID" --remove-label "$REMOVE_LABEL" -R "$REPO" || true
-    #gh issue view "$ISSUE_ID" --json labels --jq '.labels|.[]|.name' -R "$REPO"
-    set +x
 fi
 
 if [ "$ADD_LABEL" != "" ]; then
-    set -x
-    #gh issue view "$ISSUE_ID" --json labels --jq '.labels|.[]|.name' -R "$REPO"
     gh issue edit "$ISSUE_ID" --add-label "$ADD_LABEL" -R "$REPO"
-    #gh issue view "$ISSUE_ID" --json labels --jq '.labels|.[]|.name' -R "$REPO"
-    set +x
+fi
+
+if [ "$SET_LABEL" != "" ]; then
+    gh issue edit "$ISSUE_ID" --remove-label "$CONFIG_all_labels" -R "$REPO"
+    gh issue edit "$ISSUE_ID" --add-label "$SET_LABEL" -R "$REPO"
 fi
